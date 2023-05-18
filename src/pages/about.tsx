d9e1082460cf1e1f4bd23a11cfbc1cf93789a3ff
Argox2 <gaelrojotrujillo@gmail.com>
2023-05-18 12:41:55 -0600

    Part 3 of the tutorial of Gatsby

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function AboutPage() {
   return(
     <Layout pageTitle="About Me">
       <p>This is a my about page.</p>
+      <StaticImage 
+        alt='A Gray cat, very beatiful'
+        src='../images/cat.jpg'
+      />
+
     </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import {StaticImage} from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/layout';
 
@@ -6,6 +7,10 @@ export default function indexPage() {
     <div>
       <Layout pageTitle='HomePage'>
         <p>This is the HomePage.</p>
+        <StaticImage 
+          alt='Clifford, a redwish-brown pitbull, posing on a couch and looking stoically at the camera'
+          src='https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large'
+        />
       </Layout>
     </div>
   )
