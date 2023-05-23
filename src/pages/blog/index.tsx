826cdcf488d23aae0147e04996e58925e9178f69
Argox2 <gaelrojotrujillo@gmail.com>
2023-05-23 10:30:27 -0600

    Part 6

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 interface Node {
   frontmatter: {
     date: string;
     title: string;
+    slug: string;
   };
   id: string;
   excerpt: string;
@@ -26,7 +27,11 @@ const BlogPage: React.FC<BlogPageProps> = ({ data }) => {
       {
         data.allMdx.nodes.map((node: Node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link> 
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
             <p>{node.excerpt}</p>
           </article>
@@ -43,6 +48,7 @@ export const query = graphql`
       frontmatter {
         date(formatString: "MMMM D, YYYY")
         title
+        slug
       }
       id
       excerpt
