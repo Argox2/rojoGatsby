727bf618fcc6eb761220a5512633670db2962cea
Argox2 <gaelrojotrujillo@gmail.com>
2023-05-24 12:07:49 -0600

    Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react'
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 interface BlogPostProps {
   data: {
     mdx: {
-      id: string;
       frontmatter: {
         title: string;
         date: string;
+        hero_image_alt: string;
+        hero_image_credit_link: string;
+        hero_image_credit_text: string;
+        hero_image: {
+          childImageSharp: {
+            gatsbyImageData: any;
+          }
+        }
       }
     }
   }
@@ -17,9 +25,22 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.FC<BlogPostProps> = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      {image && (
+        <GatsbyImage
+          image={image}
+          alt={data.mdx.frontmatter.hero_image_alt}
+        />
+      )}
+      <p>
+        Photo Credit: {" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -28,10 +49,17 @@ const BlogPost: React.FC<BlogPostProps> = ({ data, children }) => {
 export const query = graphql`
   query ($id:String) {
     mdx(id: {eq: $id}) {
-      id
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM D, YYYY")        
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
